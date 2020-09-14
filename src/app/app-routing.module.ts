48e1a90b52d78969595b5bbf6dd75f3ad004b75e
Vinícius S. Chab <vchab@gatefy.com>
2020-09-14 19:52:17 -0300

    Estrutura de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { HomeComponent } from "./main/home/components/home/home.component";
+
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalendarComponent
+  ],
+  exports: [
+    CalendarComponent
+  ],
   imports: [
     CommonModule
   ]
